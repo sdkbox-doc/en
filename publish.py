b5cbcf0b2e070332eb35c9a98ce896bf235f0cf6
Nite Luo <darkdukey@gmail.com>
2015-07-17 15:06:40 -0700

    update script

--- publish.py
+++ publish.py
@@ -3,9 +3,9 @@
 
 import subprocess
 
-subprocess.Popen(["git", "fetch"], cwd=template_dir).wait()
-subprocess.Popen(["git", "checkout", "gh-pages "], cwd=template_dir).wait()
-subprocess.Popen(["git", "pull"], cwd=template_dir).wait()
-subprocess.Popen(["git", "checkout", "master"], cwd=template_dir).wait()
-subprocess.Popen(["git", "pull"], cwd=template_dir).wait()
-subprocess.Popen(["mkdocs", "gh-deploy", "--clean"], cwd=template_dir).wait()
+subprocess.Popen(["git", "fetch"]).wait()
+subprocess.Popen(["git", "checkout", "gh-pages "]).wait()
+subprocess.Popen(["git", "pull"]).wait()
+subprocess.Popen(["git", "checkout", "master"]).wait()
+subprocess.Popen(["git", "pull"]).wait()
+subprocess.Popen(["mkdocs", "gh-deploy", "--clean"]).wait()
