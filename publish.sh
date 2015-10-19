81148a1d3e277ca0328506d5ef84e4dd4571f9b5
slackmoehrle <slackmoehrle@gmail.com>
2015-10-19 19:19:14 -0400

    added static file copying

--- publish.sh
+++ publish.sh
@@ -8,4 +8,13 @@ git pull
 git checkout master
 git pull
 python ./src/doc.py all
+
+plugins=('adcolony' 'agecheq' 'bee7' 'chartboost' 'facebook' 'flurryanalytics'
+'fyber' 'googleanalytics' 'iap' 'kochava' 'review' 'soomlagrow' 'tune'
+'valuepotion' 'vungle')
+
+for i in ${plugins[@]}; do
+    cp docs/plugins/${i}/index.html site/plugins/${i}/.
+done
+
 mkdocs gh-deploy #--clean
